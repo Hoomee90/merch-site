af909770a0077c8601cbb4178df329af713b6ebd
Samantha Callie <Hoomee90@gmail.com>
2024-12-12 13:25:53 -0800

    add state control to site

---
+++ src/components/ItemControl.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class ItemControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      mainItemList: []
+    };
+  }
+
+  handleAddingNewItemToList = (newItem) => {
+    const newMainItemList = this.state.mainItemList.concat(newItem);
+    this.setState({
+      mainItemList: newMainItemList,
+      formVisibleOnPage: false
+    });
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+
+  render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      buttonText = "Return to Merchandise List";
+    } else {
+      buttonText = "Add Item";
+    }
+    return (
+      <React.Fragment>
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default TicketControl;
