e4522db6e7d7931034420a81ec2ce2e73f05c683
Samantha Callie <Hoomee90@gmail.com>
2024-12-12 14:18:05 -0800

    add header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
 import ItemControl from "./ItemControl";
+import Header from "./Header";
 
 function App() {
   return (
     <React.Fragment>
       <Container fluid>
+        <Header />
         <ItemControl />
       </Container>
     </React.Fragment>
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Nondescript Merchandise Store</h1>
+    </React.Fragment>
+  );
+}
+
+export default Header;
