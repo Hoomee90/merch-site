04c1362d38db4ba8133ab769bfd1b26f39e1892d
Samantha Callie <Hoomee90@gmail.com>
2024-12-12 13:53:17 -0800

    fix imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
+import ItemControl from "./ItemControl";
 
 function App() {
   return (
     <React.Fragment>
       <Container fluid>
-
+        <ItemControl />
       </Container>
     </React.Fragment>
   );
